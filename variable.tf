1a1c92e2b67437ea04aff182799066ea41d9c121
GusLuck <117812179+GusLuck@users.noreply.github.com>
2023-01-16 15:43:35 +0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "location" {
+    default = "East US 2"
+}
+
+variable "name" {
+    default = "windowsRG"
+}
+variable "username" {
+    default = "adminuser"
+}
+
+variable "windowsRG" {
+    type = string
+    description = "windowsRG1"
+}
+variable "VM" {
+    default = "WindowsVM"
+}
+variable "user" {
+    default = "adminuser"
+}
+variable "pass" {
+    default = "S3r@S3r@1234"
+}
